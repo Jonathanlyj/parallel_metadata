cd1bab3e5eac59bd9e253754ff5e0a91379b138f
Jonathanlyj <youjia@northwestern.edu>
2025-08-02 21:09:34 -0700

    clean up repo

--- run_app.sh
+++ run_app.sh
@@ -1,5 +1,9 @@
 export PNETCDF_HINTS="nc_hash_size_dim=1048576;nc_hash_size_var=1048576"
-mpiexec -n 4 ./app_baseline_test_all data/dataset_568k_metadata ./data/dataset_568k_metadata_app_baseline_test_all.h5
-# mpiexec -n 4 ./app_baseline_test_all data/dataset_5m_metadata ./data/dataset_5m_metadata_app_baseline_test_all.nc
+mpiexec -n 4 ./app_baseline_test_all data/dataset_568k_metadata ./data/dataset_568k_metadata_app_baseline_test_all.nc
+
+export PNETCDF_HINTS="nc_hash_size_dim=16777216;nc_hash_size_var=16777216"
+mpiexec -n 4 ./app_baseline_test_all data/dataset_5m_metadata ./data/dataset_5m_metadata_app_baseline_test_all.nc
+
 # read the created file
-# mpiexec -n 4 ./app_baseline_read_test_all /files2/scratch/yll6162/parallel_metadata/app_baseline_test_all.nc
+export PNETCDF_HINTS="nc_hash_size_dim=1048576;nc_hash_size_var=1048576"
+mpiexec -n 4 ./data/dataset_568k_metadata_app_baseline_test_all.nc
